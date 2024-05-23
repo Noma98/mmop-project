fd3bddbec20fb6cdba689d2dd631dbda8488687f
Noma <wiostz98kr@gmail.com>
2024-05-23 14:49:32 +0900

    Init: Set initial configuration(next+typescript+jest+tailwindcss+eslint)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
+import '@/app/global.css';
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
   );
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: 'Home',
+};
+
+export default function HomePage() {
+  return <h1>Home page</h1>;
+}
