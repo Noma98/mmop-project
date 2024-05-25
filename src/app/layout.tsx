94f955733c4f02b766a8a27013580b37532f75fd
Noma <wiostz98kr@gmail.com>
2024-05-25 11:58:39 +0900

    Feat: Add pretendard font and metadata to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,30 @@
+import { Metadata } from 'next';
+import localFont from 'next/font/local';
+
 import '@/app/global.css';
 import AuthContext from '@/app/context/AuthContext';
 
+const pretendard = localFont({
+  src: '../../public/font/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '100 700',
+  variable: '--font-pretendard',
+});
+export const metadata: Metadata = {
+  title: {
+    default: 'mmop',
+    template: '%s | mmop',
+  },
+  description: 'Portfolio creation web services',
+};
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>
+    <html lang='en' className={`${pretendard.variable}`}>
+      <body className={`${pretendard.className}`}>
         <AuthContext>
           <main>{children}</main>
         </AuthContext>
