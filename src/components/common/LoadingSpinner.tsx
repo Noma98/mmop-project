9d0d683c5d6ea34bfe2194a26759fcde22659b68
Noma <wiostz98kr@gmail.com>
2024-06-05 12:35:10 +0900

    Mod: Add props to LoadingSpinner (isPadding)

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -4,13 +4,19 @@ import { PulseLoader } from 'react-spinners';
 type Props = {
   color?: string;
   size?: string;
+  isPadding?: boolean;
 };
 export default function LoadingSpinner({
   color = '#4f90c2',
   size = '10px',
+  isPadding = true,
 }: Props) {
   return (
-    <section className='w-full h-full flex justify-center items-center py-40'>
+    <section
+      className={`w-full h-full flex justify-center items-center ${
+        isPadding ? 'py-40' : ''
+      }`}
+    >
       <PulseLoader color={color} size={size} />
     </section>
   );
