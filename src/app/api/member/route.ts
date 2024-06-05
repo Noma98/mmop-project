b8a91fbdeae25b7f2dfc517eb3807805b2f0ba58
Noma <wiostz98kr@gmail.com>
2024-06-05 12:40:16 +0900

    584cd9fb)Feat: Implement UserInfoForm to SettingPage
    - missing commit

---
+++ src/app/api/member/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { updateMember } from '@/service/member';
+import { withSessionUser } from '@/utils/session';
+
+export async function PUT(req: NextRequest) {
+  return withSessionUser(async () => {
+    const formData = await req.formData();
+    const data = JSON.parse(formData.get('data')?.toString() as string);
+    const file = (formData.get('file') as File) || null;
+
+    return updateMember(data, file)
+      .then((res) => NextResponse.json(res))
+      .catch((err) => new Response(JSON.stringify(err), { status: 500 }));
+  });
+}
