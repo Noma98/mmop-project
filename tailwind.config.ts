a030d5c416e01e3d26b83bf847f26ddacf70777e
Noma <wiostz98kr@gmail.com>
2024-06-07 14:08:15 +0900

    Design: Add global tailwind style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,22 @@ const config: Config = {
       fontFamily: {
         flower: ['Indie Flower', 'cursive'],
       },
+      colors: {
+        base: '#F9F7F7',
+        point1: '#DCF2F1',
+        point2: '#D0E9F7',
+        point3: '#7FC7D9',
+        deep1: '#3F72AF',
+        deep2: '#112D4E',
+      },
+      backgroundColor: {
+        base: '#F9F7F7',
+        point1: '#DCF2F1',
+        point2: '#D0E9F7',
+        point3: '#7FC7D9',
+        deep1: '#3F72AF',
+        deep2: '#112D4E',
+      },
     },
   },
   plugins: [],
