99822196bc3961b4ba36024ddbbacae76e7203ae
Noma <wiostz98kr@gmail.com>
2024-06-11 11:08:18 +0900

    Chore: Set environment variables for testing

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 const nextJest = require('next/jest');
+const dotenv = require('dotenv');
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -10,7 +11,8 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
   clearMocks: true,
+  setupFiles: ['dotenv/config'],
 };
-
+dotenv.config({ path: './.env.test' });
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
