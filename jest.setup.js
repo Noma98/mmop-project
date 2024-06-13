166d985809df09369e88570c5a79e77b2b015aef
Noma <wiostz98kr@gmail.com>
2024-06-13 15:27:56 +0900

    Chore: Set certain errors not visible to the terminal

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,15 @@
 // Learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom";
+import '@testing-library/jest-dom';
+
+const originalConsoleError = console.error;
+console.error = (message, ...args) => {
+  const messageStr = JSON.stringify(message);
+  if (
+    !Boolean(
+      messageStr.includes('Warning: `ReactDOMTestUtils.act` is deprecated') ||
+        messageStr.includes('unhandled exception')
+    )
+  ) {
+    originalConsoleError(message, ...args);
+  }
+};
